7446dc94dbf0e3e0df3904225a4a2df66d76487c
Resource-02 <resource2.code4bharat@gmail.com>
2025-03-22 08:16:48 +0000

    adjustment in session page jsx

--- src/app/sessionmanagement/page.jsx
+++ src/app/sessionmanagement/page.jsx
@@ -1,13 +1,27 @@
-"use client"
-import SessionManagement from '@/components/SessionManagement/SessionManagement'
-import React from 'react'
+"use client";
+import Navbar from "@/components/Layout/Navbar";
+import AdminSidebar from "@/components/Layout/Sidebar";
+import SessionManagement from "@/components/SessionManagement/SessionManagement";
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className='w-5/6 md:block' >
-        <SessionManagement />
-    </div>
-  )
-}
+    <div className="h-screen flex flex-col">
+      <div className="w-full">
+        <Navbar />
+      </div>
 
-export default page
+      <div className="flex flex-1">
+        <div className="w-1/5 min-w-[250px] mt-10 ml-4">
+          <AdminSidebar />
+        </div>
+
+        <div className="flex-1 p-6">
+          <SessionManagement />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
