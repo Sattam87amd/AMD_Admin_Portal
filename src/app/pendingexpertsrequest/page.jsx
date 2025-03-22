338e4b5f9302c5668137d931bc81bc988d7bf04f
Resource-02 <resource2.code4bharat@gmail.com>
2025-03-22 11:36:57 +0000

    Created Expert page and PendingExpertsRequest page

---
+++ src/app/pendingexpertsrequest/page.jsx
@@ -0,0 +1,13 @@
+import PendingExpertsRequest from '@/components/PendingExpertsRequest/PendingExpertsRequest'
+import Experts from '@/components/PendingExpertsRequest/PendingExpertsRequest'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <PendingExpertsRequest />
+    </div>
+  )
+}
+
+export default page
