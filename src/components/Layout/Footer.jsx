99c6d8d2b285f65aa52b4b6232cc5d713d05a688
Resource-03 <resource3.code4bharat@gmail.com>
2025-03-23 21:23:45 +0530

    changes done in footer and the usermanagement page

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,32 +1,32 @@
 const Footer = () => {
-    return (
-      <footer className="bg-gray-200 text-gray-700 text-sm p-4 flex justify-between items-center h-[80px] w-[1170px] mx-88 px-3 border-md rounded">
-        {/* Left Section */}
+  return (
+    <footer className="bg-gray-200 text-gray-700 text-sm py-4 px-6 flex flex-row h-24 justify-between items-center border-t rounded-t-2xl ">
+      {/* Left Section */}
+      <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4">
+        <p>Copyright © 2025 AMD. All rights reserved.</p>
         <div className="flex items-center space-x-2">
-          <p>Copyright © 2025 AMD. All rights reserved.</p>
-          
           <a
             href="/terms-of-use"
-            className="text-blue-600 hover:underline cursor-pointer mx-44"
+            className="text-blue-600 hover:underline"
           >
             Terms of Use
           </a>
-          <span className="text-gray-400 -mx-44">|</span>
+          <span className="text-gray-400">|</span>
           <a
             href="/privacy-policy"
-            className="text-blue-600 hover:underline cursor-pointer mx-48"
+            className="text-blue-600 hover:underline"
           >
             Privacy Policy
           </a>
         </div>
-  
-        {/* Right Section */}
-        <div className="text-gray-700">
-          <p>Seek insights that are valuable to you</p>
-        </div>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+      </div>
+
+      {/* Right Section */}
+      <div className="text-gray-700 mt-2 md:mt-0">
+        <p>Seek insights that are valuable to you</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
