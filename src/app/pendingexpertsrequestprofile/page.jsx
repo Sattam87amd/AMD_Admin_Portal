27a64ac77bf638ccef91b01b281a72ee9a3f8b31
Resource-04 <resource4.code4bharat@gmail.com>
2025-03-27 16:11:03 +0530

    added red border in experts page.

--- src/app/pendingexpertsrequestprofile/page.jsx
+++ src/app/pendingexpertsrequestprofile/page.jsx
@@ -1,29 +1,45 @@
+'use client'
+
 import Footer from '@/components/Layout/Footer'
 import Navbar from '@/components/Layout/Navbar'
 import AdminSidebar from '@/components/Layout/Sidebar'
 import PendingExpertsRequestProfile from '@/components/PendingExpertsRequestProfile/PendingExpertsRequestProfile'
+// import PendingExpertsRequest from '@/components/PendingExpertsRequest/PendingExpertsRequest'
+// import Withdrawal from '@/components/Withdrawal/Withdrawal'
+// import Transaction from '@/components/Transaction/Transaction'
 import React from 'react'
 
-
-const page = () => {
+const Page = () => {
   return (
-    <div>
-       
-      <div className='h-[90px]'>
-      <Navbar/>
+    <div className="flex flex-col min-h-screen">
+      {/* Top Navbar */}
+      <Navbar />
+
+      {/* Main Content Area */}
+      <div className="flex flex-1">
+        {/* Sidebar (Left) */}
+        <div className="hidden lg:block w-64">
+          <AdminSidebar />
+        </div>
+
+        {/* Red Divider (optional) */}
+        <div className="hidden lg:block w-1 bg-red-600 ml-8" />
+
+        {/* Main Content (Right) */}
+        <div className="flex-1  overflow-auto">
+          <PendingExpertsRequestProfile/>
+          <div>
+          
+          <Footer />
+          </div>
+        </div>
+        
       </div>
 
-      <div className='flex '> 
-        <div className='w-[300px]'>
-      <AdminSidebar/>
-        </div>
-      <PendingExpertsRequestProfile/>
-      </div>
-      
-      <Footer/>
+      {/* Footer at Bottom */}
       
     </div>
   )
 }
 
-export default page
+export default Page;
