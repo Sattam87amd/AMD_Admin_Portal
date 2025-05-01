0f9c273a590ca501f85dddf698cbe3efd20277b3
Resource-02 <resource2.code4bharat@gmail.com>
2025-05-01 10:13:13 +0530

    Refactor user management page layout: update sidebar, main content, and footer positioning

--- src/app/usermanagement/page.jsx
+++ src/app/usermanagement/page.jsx
@@ -21,15 +21,19 @@ const Page = () => {
         <div className="hidden lg:block w-1 bg-red-600 ml-8" />
 
         {/* Main Content (Right) */}
-        <div className="flex-1 overflow-auto">
+        <div className="flex-1 overflow-auto pb-16">
           <UserManagement />
         </div>
       </div>
 
-      {/* Footer at the bottom */}
-      {/* <div className="mt-auto w-[calc(80%+3.225rem)] ml-auto"> */}
-      <div className='fixed bottom-0 w-[88.5rem] ml-[18.3rem]'>
-        <Footer />
+      {/* Footer at the bottom - aligned with main content only */}
+      <div className="flex mt-auto">
+        {/* Empty space to align with sidebar */}
+        <div className="hidden lg:block w-64 ml-8"></div>
+        {/* Footer aligned with main content */}
+        <div className="flex-1">
+          <Footer />
+        </div>
       </div>
     </div>
   )
